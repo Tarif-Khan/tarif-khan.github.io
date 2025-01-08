564f64ac24abcdd6050e9e7f994f83930a0a955f
tarifkhan <khan.ta@northeastern.edu>
2025-01-07 22:49:37 -0500

    push

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import App from './App'; // Correct relative path for App.tsx
+import './index.css'; // Correct relative path for index.css
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
