6290709650b82f199496d27057d73ffa819e8f17
tarifkhan <khan.ta@northeastern.edu>
2025-01-08 01:06:52 -0500

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Projects from './pages/Projects';
 
 function App() {
   return (
-    <Router basename="/">
+    <Router>
       <div className="page-container">
         <Navbar />
         <main>
