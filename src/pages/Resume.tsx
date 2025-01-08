3e8ec3d0d0ed1db8741c5efd79fae5e436e37288
tarifkhan <khan.ta@northeastern.edu>
2025-01-07 19:24:43 -0500

    PDF Finally works YAY!

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import PDFViewer from '../components/PDFViewer';
-import '../styles/experiences.css';
 import '../styles/resumeview.css';
 
 const Resume = () => {
-  const pdfUrl = "/Khan Tarif Resume.pdf";
+  const pdfUrl = "/Khan_Tarif_Resume.pdf";
+
   return (
     <div className="content-wrapper">
       <h2 className="section-title">Resume</h2>
-      <PDFViewer pdfUrl={pdfUrl} />
+      <iframe
+        src={pdfUrl}
+        title="Resume PDF"
+        style={{ width: '100%', height: '100vh', border: 'none' }}
+      ></iframe>
     </div>
   );
 };
